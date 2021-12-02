e35dc3bda9331457cdc8abdc9e1a5447961b8066
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-12-02 10:21:33 -0400

    quick install file

---
+++ install.sh
@@ -0,0 +1,7 @@
+php artisan db:wipe
+php artisan migrate
+php artisan passport:install
+php artisan db:seed
+# composer dump-autoload
+# php artisan db:seed --class=DatabaseSeederProducts
+#php artisan serve | php artisan websockets:serve
