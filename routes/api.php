dadeaac14d84e2d827fb8534c7c2f22610ec5392
Jesús Rogliero <jesus.m.423@hotmail.com>
2021-12-02 11:53:36 -0400

    routes to login and create users

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+# Grupo de rutas para la autenticacion
+Route::group( ['prefix' => "auth"], function() {
+	Route::post('login', 'AuthController@login');
+	Route::post('signup', 'AuthController@signup');
+});
+
+
+# rutas para usuarios autenticados
+Route::group(['middleware' => ['auth:api']], function () {
+	
+	Route::resource("users", "UsersController");
+
+	
 });
