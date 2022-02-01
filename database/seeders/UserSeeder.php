611335e843575b3f55a5aacca57fcd2abae2dd1c
jesmirou <jesmirou@gmail.com>
2022-02-01 18:37:49 -0400

    seed to create users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\user;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = [
+            [
+                'name' => 'Jesus Miguel',
+                'lastname' => 'Rogliero Colmenares',
+                'email' => 'admin@api.com',
+                'password' => Hash::make('Jesus24may99!')
+            ],
+
+            [
+                'name' => 'Patricia',
+                'lastname' => 'Rogliero Colmenares',
+                'email' => 'admin1@api.com',
+                'password' => Hash::make('Jesus24may99!')
+            ],
+
+        ];
+
+
+        foreach ($users as $user) {
+            User::firstOrCreate($user, $user);
+        }
+    }
+}
