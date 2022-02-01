1de0602978b84e235913c5a0d5a33c311b2eb860
jesmirou <jesmirou@gmail.com>
2022-02-01 18:38:31 -0400

    seed calls

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Role;
-use App\Models\Presentation;
-use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
@@ -17,55 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->createNewUsers();
-        $this->createNewRoles();
-        $this->createNewPresentations();
+        $this->call([
+            ProvincesSeeder::class,
+            UserSeeder::class,
+            RoleSeeder::class,
+            PresentationSeeder::class,
+            UsersRoleSeeder::class,
+            PositionsSeeder::class
+        ]);
     }
 
-
-    public function createNewUsers() {
-
-        $users = [
-            [
-                'name' => 'Jesus Miguel',
-                'lastname' => 'Rogliero Colmenares',
-                'email' => 'admin@api.com',
-                'password' => Hash::make('Jesus24may99!')
-            ]
-        ];
-
-
-        foreach ($users as $user) {
-            User::firstOrCreate($user, $user);
-        }
-    }
-
-    //roles de usuarios
-    public function createNewRoles() {
-        $roles = [
-            ['name' => 'Gerente'],
-            ['name' => 'administrador'],
-            ['name' => 'Cordinador de Almacen'],
-            ['name' => 'Cordinador de Produccion'],
-            ['name' => 'Cordinador de Administracion']
-        ];
-       
-        foreach ($roles as $role) {
-            Role::firstOrCreate($role, $role);
-        }
-    }
-
-    //presentaciones de los productos en el inventario
-    public function createNewPresentations() {
-        $presentations = [
-            ['name' => 'GAL'],
-            ['name' => 'UNID'],
-            ['name' => 'MTS'],
-            ['name' => 'LTS']
-        ];
-        
-        foreach ($presentations as $presentation) {
-            Presentation::firstOrCreate($presentation, $presentation);
-        }
-    }
 }
