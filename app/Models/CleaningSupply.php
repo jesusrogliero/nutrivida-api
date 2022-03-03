919540545b1bccf36d245d3ef314a84b364c55d3
jesmirou <jesmirou@gmail.com>
2022-03-02 22:21:59 -0400

    name change

--- app/Models/CleaningSupply.php
+++ app/Models/CleaningSupply.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class CleaningSuply extends Model
+class CleaningSupply extends Model
 {
     use HasFactory;
 
