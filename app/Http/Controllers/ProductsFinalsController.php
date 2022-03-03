96b0cccada6f81c2d22d7a2423eb847007580130
jesmirou <jesmirou@gmail.com>
2022-03-02 21:20:51 -0400

    final product controller

---
+++ app/Http/Controllers/ProductsFinalsController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Gridbox;
+use App\Models\ProductsFinal;
+
+class ProductsFinalsController extends Controller
+{
+   /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        try {
+            $params = $request->all();
+
+            #establezco los campos a mostrar
+            $params["select"] = [
+                ["field" => "products_finals.id"],
+                ["field" => "name", "conditions" => "products_finals.name"],
+                ["field" => "stock", "conditions" => "products_finals.stock"],
+                ["field" => "products_finals.created_at"],
+                ["field" => "products_finals.updated_at"]
+            ];
+            
+            # Obteniendo la lista
+            $primaries_products = Gridbox::pagination("products_finals", $params, false, $request);
+            return response()->json($primaries_products);
+        } catch(\Exception $e) {
+            \Log::info("Error  ({$e->getCode()}):  {$e->getMessage()}  in {$e->getFile()} line {$e->getLine()}");
+            return \Response::json([
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 422);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            if( empty( $request->name) )
+                throw new \Exception("Debes ingresar el nombre del producto", 1);
+
+            if( empty( $request->stock ))
+                throw new \Exception("Debes ingresar la existencia del producto", 1);
+
+            if( $request->stock < 0 )
+                throw new \Exception("Debes ingresar una existencia correcta", 1);
+                
+            $new_product = new ProductsFinal([
+                'name' => $request->name,
+                'stock' => $request->stock
+            ]);
+
+            $new_product->save();
+
+            return response()->json('Agregado Correctamente', 201);
+
+
+        } catch(\Exception $e) {
+            \Log::info("Error  ({$e->getCode()}):  {$e->getMessage()}  in {$e->getFile()} line {$e->getLine()}");
+            return \Response::json([
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 422);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        try {
+                
+            $product_final = ProductsFinal::findOrFail($id);
+            return response()->json($product_final);
+
+        } catch(\Exception $e) {
+            \Log::info("Error  ({$e->getCode()}):  {$e->getMessage()}  in {$e->getFile()} line {$e->getLine()}");
+            return \Response::json([
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 422);
+        }
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        try {  
+            throw new \Exception("Aun no esta permitido actualizar un producto", 1);
+        
+        } catch(\Exception $e) {
+            \Log::info("Error  ({$e->getCode()}):  {$e->getMessage()}  in {$e->getFile()} line {$e->getLine()}");
+            return \Response::json([
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 422);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {  
+            throw new \Exception("Aun no esta permitido eliminar un producto", 1);
+        
+        } catch(\Exception $e) {
+            \Log::info("Error  ({$e->getCode()}):  {$e->getMessage()}  in {$e->getFile()} line {$e->getLine()}");
+            return \Response::json([
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+                'message' => $e->getMessage(),
+                'code' => $e->getCode()
+            ], 422);
+        }
+    }
+}
