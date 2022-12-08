0880c0623ffa4f90febd5aeae3d88b5734742385
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2022-12-08 13:36:04 -0400

    instalacion y instrucciones del proyecto

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
+composer install
 php artisan db:wipe
 php artisan migrate
 php artisan passport:install
 php artisan db:seed
-# composer dump-autoload
-# php artisan db:seed --class=DatabaseSeederProducts
-#php artisan serve | php artisan websockets:serve
+php artisan serve
