cf372c50847351e4bbad74a6be6c32f98f336033
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2023-02-10 14:13:57 -0400

    se crea los usuarios y se le asigna un rol

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,33 +15,36 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $users = [
-            [
-                'name' => 'Jesus Miguel',
-                'lastname' => 'Rogliero Colmenares',
-                'email' => 'admin@api.com',
-                'password' => Hash::make('Jesus24may99!')
-            ],
 
-            [
-                'name' => 'Patricia',
-                'lastname' => 'Rogliero Colmenares',
-                'email' => 'admin1@api.com',
-                'password' => Hash::make('Jesus24may99!')
-            ],
+        $user1 =  User::create([
+            'name' => 'Jesus Miguel',
+            'lastname' => 'Rogliero Colmenares',
+            'email' => 'admin@api.com',
+            'password' => Hash::make('Jesus24may99!'),
+        ])->assignRole('super_admin');
 
-            [
-                'name' => 'Profesor',
-                'lastname' => 'Edgar',
-                'email' => 'admin2@api.com',
-                'password' => Hash::make('manager')
-            ],
-
-        ];
+        $user2 =  User::create([
+            'name' => 'Patricia',
+            'lastname' => 'Rogliero Colmenares',
+            'email' => 'admin1@api.com',
+            'password' => Hash::make('Jesus24may99!'),
+        ])->assignRole('Manager');
 
 
-        foreach ($users as $user) {
-            User::firstOrCreate($user, $user);
-        }
+        $user3 =  User::create([
+            'name' => 'Fabio',
+            'lastname' => 'Trentin',
+            'email' => 'admin2@api.com',
+            'password' => Hash::make('manager')
+        ])->assignRole('Warehouse_Coordinator');
+
+        
+        $user4 =  User::create([
+            'name' => 'Jesus',
+            'lastname' => 'Rogliero',
+            'email' => 'admin3@api.com',
+            'password' => Hash::make('manager')
+        ])->assignRole('Production_Coordinator');
+
     }
 }
