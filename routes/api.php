a4aee86fc08b31effd87067345601f40141c1093
Jesús Rogliero <84638984+jesusrogliero@users.noreply.github.com>
2023-02-10 14:14:28 -0400

    reorganizacion de las rutas del api

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Middleware\RolesMiddleware;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -30,58 +28,70 @@ Route::group(['middleware' => ['auth:api']], function () {
 
 	Route::get('get_session', 'AuthController@getSession');
 	Route::get('get_user', 'AuthController@getUser');
-	Route::resource('roles', 'RolesController');
 	
 	Route::resource('employes', 'EmployesController');
 	Route::get('get_provinces', 'EmployesController@show_provinces');
+	Route::get('get_positions', 'EmployesController@show_positions');
 	Route::get('get_cities_of_provinces/{province_id}', 'EmployesController@show_cities_of_provinces');
+
 	Route::resource('militiamen', 'MilitiamenController');
 
-	Route::get('get_positions', 'EmployesController@show_positions');
 	Route::resource('machineries_consumables', 'MachineriesConsumablesController');
 	Route::resource('cleaning_Supplies', 'CleaningSuppliesController');
 	Route::get('presentations', 'CleaningSuppliesController@show_presentations');
 	Route::resource('cleaning_tools', 'CleaningToolsController');
-	Route::resource('primaries_products', 'PrimariesProductsController');
-	Route::get('get_all_primaries_products', 'PrimariesProductsController@get_all_primaries_products');
+
 	Route::get('primaries_products_histories', 'PrimariesProductsHistories@index');
-	Route::resource('nonconforming_products', 'NonconformingProductsController');
-
-	Route::resource('providers', 'ProvidersController');
-
-	Route::resource('products_finals', 'ProductsFinalsController');
-	Route::resource('supplies_minors', 'SuppliesMinorsController');
-	Route::resource('supplies_minors_noconform', 'SuppliesMinorsNoconformController');
+	
 	Route::get('transactions', 'TransactionsController@index');
 
+	Route::resource("users", "UsersController");
+	
+	Route::resource("types_identities", "TypesIdentitiesController");
 
-	Route::group(['middleware' => [RolesMiddleware::class]], function() {
-		# Orden de Ingreso
-		Route::resource('purchases_orders', 'PurchasesOrderController');
-		Route::get('get_purchases_orders_items/{id}', 'PurchasesOrdersItemsController@index');
-		Route::post('purchases_orders_items', 'PurchasesOrdersItemsController@store');
-		Route::get('purchases_orders_items/{id}', 'PurchasesOrdersItemsController@show');
-		Route::put('purchases_orders_items/{id}', 'PurchasesOrdersItemsController@update');
-		Route::delete('purchases_orders_items/{id}', 'PurchasesOrdersItemsController@destroy');
-		Route::get('approve_purchase/{id}', 'PurchasesOrderController@approve');
-		Route::post('set_observation/{id}', 'PurchasesOrderController@set_observation');
-	});
+//---------------------------------------------------------------------------------------------------------------
 
+	// Administracion
+	Route::resource('providers', 'ProvidersController');
+
+	// Almacen
+	Route::resource('products_finals', 'ProductsFinalsController')->only(['index', 'store', 'show']);
+
+	Route::resource('nonconforming_products', 'NonconformingProductsController')->only(['index', 'store', 'show']);
+
+	Route::resource('primaries_products', 'PrimariesProductsController');
+	Route::get('get_all_primaries_products', 'PrimariesProductsController@get_all_primaries_products');
+
+	Route::resource('supplies_minors', 'SuppliesMinorsController');
+	Route::resource('supplies_minors_noconform', 'SuppliesMinorsNoconformController')->only(['index', 'store', 'show']);
+
+	Route::resource('purchases_orders', 'PurchasesOrderController');
+	Route::get('approve_purchase/{id}', 'PurchasesOrderController@approve');
+	Route::post('set_observation/{id}', 'PurchasesOrderController@set_observation');
+
+	Route::get('get_purchases_orders_items/{id}', 'PurchasesOrdersItemsController@index');
+	Route::resource('purchases_orders_items', 'PurchasesOrdersItemsController')->except('index');
+
+
+	// Produccion
 	Route::resource('formulas', 'FormulasController');
 	Route::get('get_formula_items/{formula_id}', 'FormulasItemsController@get_items');
-	Route::resource('formulas_items', 'FormulasItemsController');
+	Route::resource('formulas_items', 'FormulasItemsController')->except('index');
+
 	Route::resource('lines_productions', 'LinesController');
 
 	Route::resource('productions_orders', 'ProductionsOrdersController');
+	Route::get('get_formula_with_production_order/{production_order_id}', 'ProductionsOrdersController@get_formula_with_production_order');
 
-	Route::resource('productions_consumptions', 'ProductionsConsumptionsController');
+	Route::resource('productions_consumptions', 'ProductionsConsumptionsController')->only(['store', 'show']);
+	Route::resource('productions_consumptions_items','ProductionsConsumptionsItemsController')->only(['show', 'update']);
 	Route::get('get_consumption_items/{consumption_id}', 'ProductionsConsumptionsItemsController@get_consumption_items');
-	Route::get('show_consumption_item/{consumption_id}', 'ProductionsConsumptionsItemsController@show');
-	Route::put('update_consumption_item/{consumption_id}', 'ProductionsConsumptionsItemsController@update');
 
-	Route::resource("users", "UsersController");
+	Route::resource('consumptions_supplies_minors', 'ConsumptionsSuppliesMinorsController')->only(['index', 'store', 'show']);
 
-	Route::resource("types_identities", "TypesIdentitiesController");
+	Route::resource('loss_productions', 'LossProductionsController')->except('delete');
+	Route::resource('loss_productions_items', 'LossProductionsItemsController')->only(['show', 'update']);
+	Route::get('loss_production_items/{loss_production_id}', 'LossProductionsItemsController@get_items' );
 
 	
 });
