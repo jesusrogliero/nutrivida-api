3dd606406484a0f56aa23f9e74e5b0fa609b8f72
Jesus Rogliero <jesmirou@gmail.com>
2023-02-28 20:25:19 -0400

    nuevas formulas agregadas

--- database/seeders/FormulasSeeder.php
+++ database/seeders/FormulasSeeder.php
@@ -20,7 +20,7 @@ class FormulasSeeder extends Seeder
 
             $formula = new Formula();
             $formula->name = "Chicha";
-            $formula->line_id = 1;
+            $formula->line_id = 2;
             $formula->quantity_batch = 900;
             $formula->total_formula = 900;
             $formula->save();
@@ -59,6 +59,139 @@ class FormulasSeeder extends Seeder
             $item->formula_id = $formula->id;
             $item->quantity = 0.9;
             $item->save();
+
+            // ------------------------------------------------------------
+
+            $formula = new Formula();
+            $formula->name = "Cereal";
+            $formula->line_id = 1;
+            $formula->quantity_batch = 1000;
+            $formula->total_formula = 1000;
+            $formula->save();
+
+            // HArina de Arroz
+            $item = new FormulasItem();
+            $item->primary_product_id = 1;
+            $item->formula_id = $formula->id;
+            $item->quantity = 332;
+            $item->save();
+
+            // Leche Completa
+            $item = new FormulasItem();
+            $item->primary_product_id = 2;
+            $item->formula_id = $formula->id;
+            $item->quantity = 313;
+            $item->save();
+
+            // Azucar
+            $item = new FormulasItem();
+            $item->primary_product_id = 3;
+            $item->formula_id = $formula->id;
+            $item->quantity = 350;
+            $item->save();
+
+            // Vainilla
+            $item = new FormulasItem();
+            $item->primary_product_id = 5;
+            $item->formula_id = $formula->id;
+            $item->quantity = 4;
+            $item->save();
+
+            // Premix
+            $item = new FormulasItem();
+            $item->primary_product_id = 6;
+            $item->formula_id = $formula->id;
+            $item->quantity = 1;
+            $item->save();
+
+            // ------------------------------------------------------------
+
+            $formula = new Formula();
+            $formula->name = "Fororo";
+            $formula->line_id = 2;
+            $formula->quantity_batch = 1000;
+            $formula->total_formula = 1000;
+            $formula->save();
+
+            // Leche Completa
+            $item = new FormulasItem();
+            $item->primary_product_id = 2;
+            $item->formula_id = $formula->id;
+            $item->quantity = 533.5;
+            $item->save();
+
+            // Azucar
+            $item = new FormulasItem();
+            $item->primary_product_id = 3;
+            $item->formula_id = $formula->id;
+            $item->quantity = 315.6;
+            $item->save();
+
+            // Leche Descremada
+            $item = new FormulasItem();
+            $item->primary_product_id = 4;
+            $item->formula_id = $formula->id;
+            $item->quantity = 150;
+            $item->save();
+
+            // Vainilla
+            $item = new FormulasItem();
+            $item->primary_product_id = 5;
+            $item->formula_id = $formula->id;
+            $item->quantity = 0.10;
+            $item->save();
+
+            // Suero de Leche
+            $item = new FormulasItem();
+            $item->primary_product_id = 8;
+            $item->formula_id = $formula->id;
+            $item->quantity = 0.80;
+            $item->save();
+
+            // ------------------------------------------------------------
+
+            $formula = new Formula();
+            $formula->name = "Crema De Arroz";
+            $formula->line_id = 2;
+            $formula->quantity_batch = 1000;
+            $formula->total_formula = 1000;
+            $formula->save();
+
+            // HArina de Arroz
+            $item = new FormulasItem();
+            $item->primary_product_id = 1;
+            $item->formula_id = $formula->id;
+            $item->quantity = 634.80;
+            $item->save();
+
+            // Azucar
+            $item = new FormulasItem();
+            $item->primary_product_id = 3;
+            $item->formula_id = $formula->id;
+            $item->quantity = 350;
+            $item->save();
+
+            // Vainilla
+            $item = new FormulasItem();
+            $item->primary_product_id = 5;
+            $item->formula_id = $formula->id;
+            $item->quantity = 10;
+            $item->save();
+
+            // Premix
+            $item = new FormulasItem();
+            $item->primary_product_id = 6;
+            $item->formula_id = $formula->id;
+            $item->quantity = 5;
+            $item->save();
+
+            // Suero de Leche
+            $item = new FormulasItem();
+            $item->primary_product_id = 8;
+            $item->formula_id = $formula->id;
+            $item->quantity = 0.20;
+            $item->save();
+
 
             \DB::commit();
 
